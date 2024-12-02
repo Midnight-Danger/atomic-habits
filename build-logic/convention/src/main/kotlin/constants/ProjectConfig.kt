45cd48a6ba36684fe63135b709214d55662b2a1a
Arnav Quasar <158267250+Midnight-Danger@users.noreply.github.com>
2024-12-02 20:45:45 +0530

    [Setup] Shared Module export to Swift Package Manager (#2)
    link Shared module to Swift Package Manager

--- build-logic/convention/src/main/kotlin/constants/ProjectConfig.kt
+++ build-logic/convention/src/main/kotlin/constants/ProjectConfig.kt
@@ -3,7 +3,7 @@ package constants
 object ProjectConfig {
     const val applicationId = "com.daily.atomic.habits"
     const val minSdk = 29
-    const val targetSdk = 34
+    const val targetSdk = 35
     const val versionCode = 1
     const val versionName = "1.0"
 }
